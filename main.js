7ec4253933d7a111fa5c8384c2aaa9cac6bab475
TetsuyaC0inweb <64121943+tetsu458@users.noreply.github.com>
2022-09-10 23:21:34 +0700

    Added slight effect on button, func for swap pfp

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ const createRGB = () => {
 }
 
 
-
-
 spellButton.addEventListener('click', () => {
     document.getElementsByClassName("first")[0].style.backgroundColor = createRGB();
     document.getElementsByClassName("second")[0].style.backgroundColor = createRGB();
     document.getElementsByClassName("third")[0].style.backgroundColor = createRGB();
-
 })
+
+
+// image swapper
+let pfpSource = ["./gallery/espada.png", "./gallery/bubble_tea_guy.png", "./gallery/purple_guy.png", "./gallery/vampire.png"]
+let index = 1;
+
+const swapPfp = () => {
+    if(index === pfpSource.length){
+        index = 0;
+    }
+    document.getElementById("pfp-pic").src = pfpSource[index];
+    index++;
+} 
+
+swapButton.addEventListener("click", swapPfp);
