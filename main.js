a3c9fa0097a499838b8ef949dab1df2e64724619
TetsuyaC0inweb <64121943+tetsu458@users.noreply.github.com>
2022-09-10 11:04:09 +0700

    Created color palette

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-let swapButton = document.getElementById("swap");
+let swapButton =  document.getElementById("swap");
 let spellButton = document.getElementById("spell");
 
 const randomNum = num => {
     return Math.floor(Math.random() * num)
 }
 
-console.log(randomNum(364))
+const createRGB = () => {
+    return `rgb(${randomNum(364)}, ${randomNum(364)}, ${randomNum(364)})`
+}
+
+
+
+
+spellButton.addEventListener('click', () => {
+    document.getElementsByClassName("first")[0].style.backgroundColor = createRGB();
+    document.getElementsByClassName("second")[0].style.backgroundColor = createRGB();
+    document.getElementsByClassName("third")[0].style.backgroundColor = createRGB();
+
+})
