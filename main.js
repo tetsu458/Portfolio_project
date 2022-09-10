47b4c690377c16a29353eeb909e06c25cb4b80f2
TetsuyaC0inweb <64121943+tetsu458@users.noreply.github.com>
2022-09-10 10:18:12 +0700

    Trying to publish

---
+++ main.js
@@ -0,0 +1,8 @@
+let swapButton = document.getElementById("swap");
+let spellButton = document.getElementById("spell");
+
+const randomNum = num => {
+    return Math.floor(Math.random() * num)
+}
+
+console.log(randomNum(364))
